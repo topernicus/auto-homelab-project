247a96accef2805b9d14a91c2f5fbc8d086a7e97
topernicus <chris.stumph@gmail.com>
2025-05-08 12:48:51 -0500

    Improve usage of terraform conventions

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,8 @@
-terraform {
-  required_version = ">= 0.13.0"
+provider "proxmox" {
+  pm_api_url          = "https://${var.proxmox_api.host}:${var.proxmox_api.port}/api2/json"
+  pm_api_token_id     = var.proxmox_api_token.id
+  pm_api_token_secret = var.proxmox_api_token.secret
 
-  required_providers {
-    proxmox = {
-      # LINK https://github.com/Telmate/terraform-provider-proxmox
-      source  = "telmate/proxmox"
-      version = "3.0.1-rc4"
-    }
-  }
+  # NOTE Optional, but recommended to set to true if you are using self-signed certificates.
+  pm_tls_insecure = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -95,12 +95,3 @@ variable "docker_vm_1" {
     hostid      = string
   })
 }
-
-provider "proxmox" {
-  pm_api_url          = "https://${var.proxmox_api.host}:${var.proxmox_api.port}/api2/json"
-  pm_api_token_id     = var.proxmox_api_token.id
-  pm_api_token_secret = var.proxmox_api_token.secret
-
-  # NOTE Optional, but recommended to set to true if you are using self-signed certificates.
-  pm_tls_insecure = true
-}
